1b2cee2800b426d3d4a4c9476b05548cc2d829b4
justapig9020 <justapig9020@gmail.com>
2022-01-11 15:59:54 +0800

    Add integration test for processor

--- src/processor.rs
+++ src/processor.rs
@@ -11,6 +11,53 @@ enum IssueResult {
     Stall,
 }
 
+#[cfg(test)]
+mod processor {
+    use super::*;
+    #[test]
+    fn arthmatic_unit_exec() {
+        use ArgState::*;
+        let program = vec![
+            "addi R1, R0, #100", // R1 = 100
+            "addi R2, R0, #200", // R2 = 200
+            "add R3, R1, R2",    // R3 = 300
+            "add R4, R1, R3",    // R4 = 400
+            "add R3, R4, R3",    // R3 = 700
+            "addi R1, R5, #400", // R1 = 400
+            "add R5, R1, R2",    // R5 = 600
+                                 /* R1: 400
+                                  * R2: 200
+                                  * R3: 700
+                                  * R4: 400
+                                  * R5: 600
+                                  */
+        ];
+        let mut p = Processor::new();
+        p.add_path("arth").unwrap();
+        p.add_path("arth").unwrap();
+        // TODO: Use more presice loop amount
+        for _ in 0..100 {
+            let line = p.fetch_address();
+            // TODO: Avoid with workaround NOP
+            let inst = if line >= program.len() {
+                "nop"
+            } else {
+                program[line]
+            };
+            p.next_cycle(inst).unwrap();
+        }
+        let r1 = p.register_file.read(1);
+        assert!(matches!(r1, Ready(400)));
+        let r2 = p.register_file.read(2);
+        assert!(matches!(r2, Ready(200)), "R2: {:?}", r2);
+        let r3 = p.register_file.read(3);
+        assert!(matches!(r3, Ready(700)));
+        let r4 = p.register_file.read(4);
+        assert!(matches!(r4, Ready(400)));
+        let r5 = p.register_file.read(5);
+        assert!(matches!(r5, Ready(600)));
+    }
+}
 #[derive(Debug)]
 pub struct Processor {
     pc: usize,
