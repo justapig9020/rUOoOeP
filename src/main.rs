56d19140c64219389857512868edb6e3e3054715
justapig9020 <justapig9020@gmail.com>
2022-07-25 02:14:37 +0800

    Update execute program

--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,7 @@ use crate::functional_units::factory::{Factory, Function, MemFunction};
 use std::io;
 
 fn main() -> Result<(), String> {
-    let program = vec![
-        "addi R1, R0, #0",
-        "addi R2, R0, #10",
-        "sw R1, R2, #0", // j = 0, &j == 10
-        "sw R1, R2, #4", // k = 0, &k == 14
-        "addi R3, R0, #4",
-        "addi R4, R0, #5",
-        // First iteration
-        "lw R1, R2, #0",
-        "add R1, R3, R1", // j += 4
-        "sw R1, R2, #0",
-        "lw R1, R2, #4",
-        "add R1, R4, R1", // k += 5
-        "sw R1, R2, #4",
-        // Second iteration
-        "lw R1, R2, #0",
-        "add R1, R3, R1", // j += 4
-        "sw R1, R2, #0",
-        "lw R1, R2, #4",
-        "add R1, R4, R1", // k += 5
-        "sw R1, R2, #4",
-        // Third iteration
-        "lw R1, R2, #0",
-        "add R1, R3, R1", // j += 4
-        "sw R1, R2, #0",
-        "lw R1, R2, #4",
-        "add R1, R4, R1", // k += 5
-        "sw R1, R2, #4",
-    ];
+    let program = vec!["addi R1, R0, #100", "addi R1, R1, #200", "add R2, R1, R1"];
 
     let program = program.iter().map(|i| i.to_string()).collect();
 
@@ -49,22 +21,25 @@ fn main() -> Result<(), String> {
         let unit = ff.new_unit(Function::Arithmetic);
         p.add_path(unit)?;
     }
-    let unit = ff.new_mem_unit(MemFunction::MemoryAccess);
-    p.add_mem_path(unit)?;
     let mut vm = virtual_machine::Machine::new(p, program, 20);
 
+    let mut tick = 0;
     let mut result = Ok(());
     while result.is_ok() {
-        println!("{}", vm);
+        println!("================ {tick} ================");
+        println!("{vm}");
         pause();
         result = vm.next_cycle();
+        tick += 1;
     }
 
     result = Ok(());
     while result.is_ok() {
-        println!("{}", vm);
+        println!("================ {tick} ================");
+        println!("{vm}");
         pause();
         result = vm.next_flush_cycle();
+        tick += 1;
     }
 
     println!("{:?}", result);
