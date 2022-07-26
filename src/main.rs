ff137114a5f71ab95c31f4dcede4b08fe27431bf
justapig9020 <justapig9020@gmail.com>
2022-07-27 00:42:56 +0800

    Improve tick bar display

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod virtual_machine;
 use crate::core::processor::Processor;
 use crate::functional_units::factory::{Factory, Function, MemFunction};
 use std::io;
+use termion::terminal_size;
 
 fn main() -> Result<(), String> {
     let program = vec![
@@ -31,23 +32,41 @@ fn main() -> Result<(), String> {
     let mut tick = 0;
     let mut result = Ok(());
     while result.is_ok() {
-        tick += 1;
-        println!("================ {tick} ================");
+        println!("{}", tick_bar(tick));
         print!("{vm}");
         pause();
         result = vm.next_cycle();
+        tick += 1;
     }
 
     result = Ok(());
     while result.is_ok() {
         result = vm.next_flush_cycle();
-        println!("================ {tick} ================");
+        println!("{}", tick_bar(tick));
         println!("{vm}");
         pause();
         tick += 1;
     }
 
     Ok(())
+}
+
+fn tick_bar(tick: usize) -> String {
+    let tick = tick.to_string();
+    let (col, _) = terminal_size().unwrap();
+    let bar = col as usize - tick.len() - 2;
+    let left = bar / 5;
+    let right = bar - left;
+    let make_bar = |len: usize| -> String {
+        let mut bar = String::with_capacity(len);
+        for _ in 0..len {
+            bar.push('=');
+        }
+        bar
+    };
+    let left = make_bar(left);
+    let right = make_bar(right);
+    format!("{left} {tick} {right}")
 }
 
 fn pause() {
